8fae4c26b923903063ba1f948916cc97e23df2fe
Jianfei Hu <hujianfei258@gmail.com>
2026-04-26 14:12:17 -0700

    bpe merge 1/x: naive counting.
    Signed-off-by: Jianfei Hu <hujianfei258@gmail.com>

--- cs336_basics/bpe_merge.py
+++ cs336_basics/bpe_merge.py
@@ -1,8 +1,20 @@
 from pretoken import init_token_freqmap, FrequencyMap
 
+from itertools import islice
+
+
 def bpe_merge():
     freq_map = init_token_freqmap("./data/tiny-1000.txt")
-    print(f"freq_map size {len(freq_map)}")
+    # Also the same type, even though the key is always sized 2. But it's ok.
+    byte_pair: FrequencyMap = {}
+    for token, count in freq_map.items():
+        for b in zip(token[:-1], token[1:]):
+            byte_pair[b] = byte_pair.get(b, 0) + 1
+
+    # get the highest byte pair.
+    max_byte_pair = bytes(max(byte_pair, key=byte_pair.get))
+    print(max_byte_pair.decode('utf-8'))
+    
 
 if __name__ == "__main__":
     bpe_merge()
